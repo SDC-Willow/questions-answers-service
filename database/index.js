99495439536395738d042a33ee8f32e304cfcd75
Louis <louisla1992@gmail.com>
2021-09-23 22:30:01 -0700

    API is complete

--- database/index.js
+++ database/index.js
@@ -76,7 +76,135 @@ const getAnswersPhotos = (answerIds, callback) => {
   connection.query(answersPhotos, callback);
 };
 
+// Post a question
+const insertQuestion = (questionObject, callback) => {
+  const unixTimestamp = Date.now();
+  const insertString = `
+    INSERT
+      INTO Questions
+        (
+          product_id,
+          question_body,
+          question_date,
+          asker_name,
+          asker_email,
+          reported,
+          question_helpfulness
+        )
+    VALUES
+      (
+        ${questionObject.product_id},
+        '${questionObject.question_body}',
+        ${unixTimestamp},
+        '${questionObject.asker_name}',
+        '${questionObject.asker_email}',
+        0,
+        0
+      )`;
+  connection.query(insertString, callback);
+}
+
+const insertAnswer = (answerObject, callback) => {
+  const unixTimestamp = Date.now();
+  const insertString = `
+    INSERT
+      INTO ANSWERS
+        (
+          question_id,
+          body,
+          date_written,
+          answerer_name,
+          answerer_email,
+          reported,
+          helpfulness
+        )
+      VALUES
+        (
+          ${answerObject.question_id},
+          '${answerObject.body}',
+          ${unixTimestamp},
+          '${answerObject.answerer_name}',
+          '${answerObject.answerer_email}',
+          0,
+          0
+        )`;
+  connection.query(insertString, callback);
+}
+
+const insertPhotos = (photosObject, callback) => {
+  for (let i = 0; i < photosObject.photos.length; i++) {
+    let insertString = `
+      INSERT
+        INTO PHOTOS
+          (
+            answer_id,
+            url
+          )
+        VALUES
+          (
+            ${photosObject.answer_id},
+            '${photosObject.photos[i]}'
+          )`;
+    if (i === photosObject.photos.length -1) {
+      connection.query(insertString, callback);
+    } else {
+      connection.query(insertString);
+    }
+  }
+};
+
+const incrementQuestionHelpfulness = (questionId, callback) => {
+  let incrementString = `
+    UPDATE
+      QUESTIONS
+        SET
+          question_helpfulness = question_helpfulness +1
+        WHERE
+          question_id = ${questionId}`;
+  connection.query(incrementString, callback);
+}
+
+const reportQuestion = (questionId, callback) => {
+  let reportString = `
+    UPDATE
+      QUESTIONS
+        SET
+          reported = true
+        WHERE
+          question_id = ${questionId}`;
+  connection.query(reportString, callback);
+}
+
+const incrementAnswerHelpfulness = (answerId, callback) => {
+  let incrementString = `
+  UPDATE
+    ANSWERS
+      SET
+        helpfulness = helpfulness + 1
+      WHERE
+        answer_id = ${answerId}`;
+  connection.query(incrementString, callback);
+}
+
+const reportAnswer = (answerId, callback) => {
+  let reportString = `
+    UPDATE
+      ANSWERS
+        SET
+          reported = true
+        WHERE
+          answer_id = ${answerId}`;
+  connection.query(reportString, callback);
+}
+
 module.exports = {
   getQuestions,
-  getAnswersPhotos
+  getAnswersPhotos,
+  insertQuestion,
+  insertAnswer,
+  insertPhotos,
+  incrementQuestionHelpfulness,
+  reportQuestion,
+  incrementAnswerHelpfulness,
+  reportAnswer
 };
--- server/index.js
+++ server/index.js
@@ -64,44 +64,115 @@ app.get('/qa/questions', (req, res) => {
 // POST Questions
 
 app.post('/qa/questions', (req, res) => {
-  // db.getQuestions((error, result) => {
-  //   if (error) {
-  //     console.log(error)
-  //   } else {
-  //     console.log(result);
-  //   }
-  // });
+
+  const bodyData = {
+    product_id: req.body.product_id,
+    question_body: req.body.body,
+    asker_name: req.body.name,
+    asker_email: req.body.email
+  }
+
+  db.insertQuestion(bodyData, (error, result) => {
+    if (error) {
+      console.log(error)
+      res.status(422).send('Error: Question body contains invalid entries');
+    } else {
+      res.sendStatus(201);
+    }
+  });
 })
 
 // POST Answer
 
 app.post(`/qa/questions/:question_id/answers`, (req, res) => {
-
+  const questionId = parseInt(req.params.question_id);
+  const answerData = {
+    question_id: questionId,
+    body: req.body.body,
+    answerer_name: req.body.name,
+    answerer_email: req.body.email,
+    photos: req.body.photos
+  }
+  if (req.body.body) {
+    db.insertAnswer(answerData, (error, result) => {
+      if (error) {
+        console.log(error);
+        res.status(422).send('Error: Answer body contains invalid entriesusername text is required,email must be valid');
+      } else {
+        if (req.body.photos.length > 0) {
+          let data = {
+            photos: req.body.photos,
+            answer_id: result.insertId,
+          }
+          db.insertPhotos(data, (error, result) => {
+            if (error) {
+              console.log('🎃', error);
+            } else {
+              res.sendStatus(201);
+            }
+          })
+        } else {
+          res.sendStatus(201);
+        }
+      }
+    })
+  } else {
+    res.status(422).send('Error: Answer body contains invalid entriesusername text is required,email must be valid');
+  }
 })
 
 // PUT Mark Question as Helpful
 
 app.put(`/qa/questions/:question_id/helpful`, (req, res) => {
-
+  let questionId = parseInt(req.params.question_id);
+  db.incrementQuestionHelpfulness(questionId, (error, result) => {
+    if (error) {
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(204);
+    }
+  })
 })
 
 
 // PUT Report Question
 
 app.put(`/qa/questions/:question_id/report`, (req, res) => {
-
+  let questionId = parseInt(req.params.question_id);
+  db.reportQuestion(questionId, (error, result) => {
+    if (error) {
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(204);
+    }
+  })
 })
 
 // PUT Mark Answer as Helpful
 
 app.put(`/qa/answers/:answer_id/helpful`, (req, res) => {
-
+  let answerId = parseInt(req.params.answer_id);
+  db.incrementAnswerHelpfulness(answerId, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(204);
+    }
+  })
 })
 
 // PUT Report Answer
 
 app.put(`/qa/answers/:answer_id/report`, (req, res) => {
-
+  let answerId = parseInt(req.params.answer_id);
+  db.reportAnswer(answerId, (error, result) => {
+    if (error) {
+      res.sendStatus(500);
+    } else {
+      res.sendStatus(204);
+    }
+  })
 })
 
 
